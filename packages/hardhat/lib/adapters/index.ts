ef25b030ed6ee3cacd9925b518b1149172f685a4
Anthony Martin <anthony@replaycreative.com>
2021-10-22 21:42:10 +0200

    removes unused exports

--- packages/hardhat/lib/adapters/index.ts
+++ packages/hardhat/lib/adapters/index.ts
@@ -1,18 +1,1 @@
-export {
-  BeneficiaryGovernanceAdapter,
-  ProposalStatus,
-  ProposalType,
-} from "./BeneficiaryGovernance/BeneficiaryGovernanceAdapter";
-export type {
-  BeneficiaryApplication,
-  BeneficiaryImage,
-  Proposal,
-} from "./BeneficiaryGovernance/BeneficiaryGovernanceAdapter";
-export { BeneficiaryRegistryAdapter } from "./BeneficiaryRegistry/BeneficiaryRegistryAdapter";
-export {
-  default as GrantElectionAdapter,
-  ElectionState,
-  ElectionTerm,
-  ElectionTermIntToName,
-} from "./GrantElection/GrantElectionAdapter";
-export type { ElectionMetadata } from "./GrantElection/GrantElectionAdapter";
+export * from "./HYSIBatchInteraction/HYSIBatchInteractionAdapter";
