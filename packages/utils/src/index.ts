1f5ee10adc8443eddf1b3569f5f929963d68f066
Anthony Martin <anthony@replaycreative.com>
2021-10-28 13:44:15 +0200

    Revert "Merge branch 'develop' into feature/TestPOP-kovan"
    This reverts commit d8321bde38280562341301e5957e77bb92f95ce8, reversing
    changes made to 59562cda08dd8dd6b8d7b034c84bdd89397a7f2c.

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,14 @@
+export { capitalize } from './capitalize';
 export {
   bigNumberToNumber,
   formatAndRoundBigNumber,
   numberToBigNumber,
   scaleNumberToBigNumber,
 } from './formatBigNumber';
+export { IpfsClient } from './IpfsClient/IpfsClient';
+export type { IIpfsClient, UploadResult } from './IpfsClient/IpfsClient';
+export {
+  getBytes32FromIpfsHash,
+  getIpfsHashFromBytes32,
+} from './ipfsHashManipulation';
+export { default as useFetch } from './useFetch';
