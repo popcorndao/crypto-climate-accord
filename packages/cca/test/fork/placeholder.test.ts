4a9e3971f8561ce6d7b50db39acdfadfb19ee95c
Anthony Martin <anthony@replaycreative.com>
2021-11-04 14:26:24 +0100

    adds test dir

---
+++ packages/cca/test/fork/placeholder.test.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import { network } from "hardhat";
+
+describe("placeholder", () => {
+  beforeEach(async function () {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: process.env.RPC_URL,
+            blockNumber: 12724811,
+          },
+        },
+      ],
+    });
+  });
+
+  it("should pass", () => {
+    expect(true).to.eq(true);
+  });
+});
