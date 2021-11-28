36a25d0ce3048479303b9810ae27acf734516787
Anthony Martin <anthony@replaycreative.com>
2021-11-28 17:39:10 +0100

    removes unused packages

--- packages/hardhat/lib/adapters/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./HYSIBatchInteraction/HYSIBatchInteractionAdapter";
--- packages/ui/src/components/popcorn/BatchHYSI/StatCardRow/index.tsx
+++
@@ -1,7 +0,0 @@
-export const StatCardRow = ({ children }): JSX.Element => {
-  return (
-    <div className="relative -mt-12 mb-6 flex flex-row gap-x-4 flex-wrap justify-between grid-rows-1">
-      {children}
-    </div>
-  );
-};
